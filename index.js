a2d089b8dfed2a8a64de895aca42aa7a26144ceb
Arnur <arnurarykbayev@mail.ru>
2022-10-25 17:30:01 +0600

    add scenes to go

--- index.js
+++ index.js
@@ -7,9 +7,66 @@ expressApp.use(express.static("static"));
 expressApp.use(express.json());
 require("dotenv").config();
 
-const { Telegraf } = require("telegraf");
+/* require scenes, session */
+const { Telegraf, Composer, Scenes, session, Markup } = require("telegraf");
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
+
+function primeFunc(n) {
+  if(n < 2) {
+    console.log('Число не может быть отрицательным или меньше единицы');
+    return;
+  }
+  for(let i = 2; i < n; i++) {
+    let isPrime = true;
+    for(let j = 2; j < i; j++) {
+      if(i % j == 0) {
+        isPrime = false;
+        break
+      }
+    }
+    if(isPrime) console.log(i, 'prime')
+  }
+} 
+
+primeFunc(10)
+console.log('a'.charCodeAt(0))
+
+/* SCENES func */
+const startWizard = new Composer()
+startWizard.on('text', async (ctx) => {
+  await ctx.reply('Name:')
+  return ctx.wizard.next()
+})
+const firstName = new Composer()
+firstName.on('text', async (ctx) => {
+  await ctx.reply('lastName:')
+  return ctx.wizard.next()
+})
+const lastName = new Composer()
+lastName.on('text', async (ctx) => {
+  await ctx.reply('Chose:', Markup.inlineKeyboard([
+    [Markup.button.callback('Telegram', 'Telegram')],
+    [Markup.button.callback('WhatsApp', 'WhatsApp')]
+  ]))
+  return ctx.wizard.next()
+})
+
+const messenger = new Composer()
+messenger.action('Telegram', async (ctx) => {
+  await ctx.reply('right')
+  return ctx.scene.leave();
+})
+messenger.action('WhatsApp', async (ctx) => {
+  await ctx.reply('wrong')
+  return ctx.scene.leave();
+})
+
+
+/* Scenes declare */
+const menuScene = new Scenes.WizardScene('sceneWizard', startWizard, firstName, lastName, messenger)
+const stage = new Scenes.Stage([menuScene])
+
 
 expressApp.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/index.html"));
@@ -17,11 +74,18 @@ expressApp.get("/", (req, res) => {
 
 bot.command("start", (ctx) => {
   console.log(ctx.from);
-  bot.telegram.sendMessage(ctx.chat.id, "Привет, Чикибамбони", {});
+  bot.telegram.sendMessage(ctx.chat.id, "Привет, Танечка <3", {});
   bot.telegram.sendPhoto(ctx.chat.id, {
     source: "res/h1.jpg",
   });
 });
+
+bot.hears( "Э",
+  (ctx) => {
+    console.log(ctx.from);
+    bot.telegram.sendMessage(ctx.chat.id, "ЧО Э", {});
+  }
+);
 
 bot.hears(
   (msg) => {
@@ -34,6 +98,7 @@ bot.hears(
       "пидр",
       "пидор",
       "чмо",
+      "шмонька",
       "придурок",
       "бебр",
       "лошара",
@@ -52,7 +117,7 @@ bot.hears(
     if (includesRoughWords == true) {
       return true;
     } else {
-      return false;
+      return;
     }
   },
   (ctx) => {
@@ -142,9 +207,12 @@ bot.command("ethereum", (ctx) => {
     });
 });
 
-bot.hears("animals", (ctx) => {
+bot.command("animals", async (ctx) => {
   console.log(ctx.from);
-  let animalMessage = `great, here are pictures of animals you would love`;
+  await bot.telegram.sendPhoto(ctx.chat.id, {
+    source: "res/animals.jpg",
+  });
+  let animalMessage = `КОШЬКИ ИЛИ СОБАЧЬКИ?`;
   ctx.deleteMessage();
   bot.telegram.sendMessage(ctx.chat.id, animalMessage, {
     reply_markup: {
@@ -164,15 +232,22 @@ bot.hears("animals", (ctx) => {
   });
 });
 
-bot.action("dog", (ctx) => {
-  bot.telegram.sendPhoto(ctx.chat.id, {
+bot.action("dog", async (ctx) => {
+  await bot.telegram.sendPhoto(ctx.chat.id, {
     source: "res/dog.webp",
   });
 });
-bot.action("cat", (ctx) => {
-  bot.telegram.sendPhoto(ctx.chat.id, {
+bot.action("cat", async (ctx) => {
+  await bot.telegram.sendPhoto(ctx.chat.id, {
     source: "res/cat.webp",
   });
+  await bot.telegram.sendAnimation(ctx.chat.id, "https://tenor.com/view/koollua-cat-dancing-gif-24490580");
 });
 
+
+/* middleware to scenes */
+bot.use(session())
+bot.use(stage.middleware())
+
+bot.command('go', (ctx) => ctx.scene.enter('sceneWizard'))
 bot.launch();
